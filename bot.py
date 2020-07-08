fa3aea8ff381fd3dd8ff13981bb0bc4c19348703
iampratiktandel <iampratiktandel@gmail.com>
2020-07-08 22:14:49 +0530

    Feat: Implemented Kick and Ban Function

--- bot.py
+++ bot.py
@@ -47,5 +47,13 @@ async def _8ball(ctx, *, question):
 @client.command()
 async def clear(ctx, amount=5):
   await ctx.channel.purge(limit=amount)
-  
+
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason = None):
+  await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason = None):
+  await member.ban(reason=reason)
+
 client.run('Token goes here')
