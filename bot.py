01d9c1d182070f5cca0de2f581098d1752b90a4c
iampratiktandel <iampratiktandel@gmail.com>
2020-07-08 21:16:08 +0530

    Feat: Implemented new 8ball function

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 client = commands.Bot(command_prefix = '.')
 
@@ -10,9 +11,37 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
   print(f'{member} has joined the server.')
-
+  
 @client.event
 async def on_member_remove(member):
   print(f'{member} has left the server.')
 
-client.run('Token goes Here')
+@client.command()
+async def ping(ctx):
+  await ctx.send(f'Pong! {round(client.latency*1000)}ms')
+
+@client.command(aliases=['8ball', 'test'])
+async def _8ball(ctx, *, question):
+  responses = ['As I see it, yes.',
+              'Ask again later.',
+              'Better not tell you now.',
+              'Cannot predict now.',
+              'Concentrate and ask again.',
+              'Don’t count on it.',
+              'It is certain.',
+              'It is decidedly so.',
+              'Most likely.',
+              'My reply is no.',
+              'My sources say no.',
+              ' Outlook not so good.',
+              'Outlook good.',
+              'Reply hazy, try again.',
+              'Signs point to yes.',
+              'Very doubtful.',
+              'Without a doubt.',
+              'Yes.',
+              'Yes – definitely.',
+              'You may rely on it.']
+  await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+client.run('Token goes here')
